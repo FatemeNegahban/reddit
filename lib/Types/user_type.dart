57ac0b78cb3e25dc3881b1f6f4da5e35fed88bec
Fatemeh Roshan <famazlom@gmail.com>
2022-05-24 16:54:41 +0430

    change user_type.dart

--- lib/Types/user_type.dart
+++ lib/Types/user_type.dart
@@ -1,10 +1,11 @@
+import 'package:reddit/Types/community_type.dart';
 import 'package:reddit/Types/post_type.dart';
 class User{
   late final String userId;
   late final String _password;
   late String avatar;
   late List<Post> myPosts;
-  late List<Post> myLikes;
+  late List<Community> followings;
   User(userId,password){
     this.userId=userId;
     this._password=password;
