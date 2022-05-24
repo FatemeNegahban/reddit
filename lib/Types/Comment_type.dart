39e0d62235e2af597979ad452116329243e34b6e
Fatemeh Roshan <famazlom@gmail.com>
2022-05-24 16:28:50 +0430

    change Comment_type.dart

--- lib/Types/Comment_type.dart
+++ lib/Types/Comment_type.dart
@@ -1,3 +1,7 @@
+import 'package:reddit/Types/post_type.dart';
+import 'package:reddit/Types/user_type.dart';
 class Comment{
-
+late final Post thisPost;
+late final User commenter;
+late final String comment;
 }
