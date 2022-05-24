42f0a7f7b71d807a8914ebfd598ca31d5cdc9885
Fatemeh Roshan <famazlom@gmail.com>
2022-05-24 17:11:09 +0430

    change user_type.dart

--- lib/Types/Comment_type.dart
+++ lib/Types/Comment_type.dart
@@ -1,7 +1,8 @@
 import 'package:reddit/Types/post_type.dart';
 import 'package:reddit/Types/user_type.dart';
 class Comment{
-late final Post thisPost;
+  Comment(this.myPost,this.commenter,this.comment);
+late final Post myPost;
 late final User commenter;
 late final String comment;
 }
--- lib/Types/post_type.dart
+++ lib/Types/post_type.dart
@@ -1,6 +1,7 @@
 import 'package:reddit/Types/user_type.dart';
 import 'Comment_type.dart';
 class Post{
+  Post(this.author,this.title,this.body,this.postTime);
   late final User author;
   late final String title, body, image;
   late final DateTime postTime;
