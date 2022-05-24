0cb1167aeec55f3a8378ec26c31346b939f30bb5
Fatemeh Roshan <famazlom@gmail.com>
2022-05-24 16:49:26 +0430

    change user_type.dart

--- lib/Types/user_type.dart
+++ lib/Types/user_type.dart
@@ -1,8 +1,22 @@
 import 'package:reddit/Types/post_type.dart';
 class User{
   late final String userId;
-  late final String password;
+  late final String _password;
   late String avatar;
   late List<Post> myPosts;
   late List<Post> myLikes;
+  User(userId,password){
+    this.userId=userId;
+    this._password=password;
+  }
+  set setAvatar(String avatar){
+    this.avatar=avatar;
+  }
+  String get getAvatar{
+    return avatar;
+  }
+  String get getPassword{
+    return _password;
+  }
+
 }
