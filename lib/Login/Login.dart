40046e4320ff5bb3c1a9c14c8e56739159fc09e4
Fateme Negahban <fatemenegahban@example.com>
2022-05-24 15:06:41 +0430

    Rounded Button

---
+++ lib/Login/Login.dart
@@ -0,0 +1,35 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import '../materials/body.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        actions: [
+          ElevatedButton(
+              style: ButtonStyle(
+                  elevation: MaterialStateProperty.all(0),
+                  backgroundColor: MaterialStateProperty.all(Colors.black)
+              ),
+              onPressed: (){
+
+              },
+              child: Text("Sign up",
+                style: TextStyle(
+                  color: Colors.blue,
+                ) ,))
+        ],
+        leading: IconButton(
+          icon: Icon(Icons.close),
+          onPressed: () {  },
+        ),
+      ),
+      body: Body(key: Key("value"),),
+    );
+  }
+}
