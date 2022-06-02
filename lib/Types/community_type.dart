ea9c9eaa8194810ff0fbb541c2ae87336ed1512d
Fatemeh Roshan <famazlom@gmail.com>
2022-06-02 14:09:14 +0430

    change community_type.dart

--- lib/Types/community_type.dart
+++ lib/Types/community_type.dart
@@ -1,2 +1,7 @@
+import 'package:reddit/Types/post_type.dart';
+import 'package:reddit/Types/user_type.dart';
 class Community{
+  late final User admin;
+  late final List<User> followers;
+  late final List<Post> posts;
 }
