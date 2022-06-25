c0c5b2d6a68d3c3c9449aa04766dfea0c17c7e0b
Fateme Negahban <fatemenegahban@example.com>
2022-06-25 16:41:17 +0430

    add title is committed

--- lib/login/body.dart
+++ lib/login/body.dart
@@ -15,10 +15,17 @@ class Body extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<Body> createState() => _BodyState();
+  State<Body> createState() => _BodyState(addUser: addUser);
 }
 
 class _BodyState extends State<Body> {
+  Function addUser;
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  _BodyState({
+    required this.addUser,
+});
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -35,16 +42,20 @@ class _BodyState extends State<Body> {
                 children: <Widget>[
                   SizedBox(height: size.height * 0.03),
                   RoundedInputField(
-                    hintText: "Your Email",
-                    onChanged: (value) {}, key: Key("email_input"),
+                    hintText: "Username",
+                    onChanged: (value) {
+                      _usernameController.text = value;
+                    }, key: Key("username_input"),
                     hintStyle: Colors.white,
                   ),
                   RoundedPasswordField(
-                    onChanged: (value) {}, key: Key("value"),
+                    onChanged: (value) {
+                      _passwordController.text = value;
+                    }, key: Key("password_input"),
                   ),
                   RoundedButton(
-                    text: "LOGIN",
-                    press: () {}, key: Key("login_input"),
+                    text: "Continue",
+                    press: () {}, key: Key("continue_input"),
                   ),
                   SizedBox(height: size.height * 0.03),
                 ],
