105e5a6c97ce04a0d3cc8d9600102cfcfbe0afe6
Fateme Negahban <fatemenegahban@example.com>
2022-06-27 11:02:36 +0430

    new edited Post

--- lib/classes/Post.dart
+++ lib/classes/Post.dart
@@ -1,16 +1,16 @@
-import 'community.dart';
+import 'Community.dart';
 import 'User.dart';
 
 class Post{
   late String title;
   late String content;
   DateTime createdAt=DateTime.now();
-  community? com;
+  Community? community;
   Object? id;
   Object? image;
   int? likeCount;
   List<String>? comments;
   User? user;
 
-  Post({required this.title, required this.content, this.com, this.id, this.image, this.likeCount,this.comments,this.user});
+  Post({required this.title, required this.content, this.community, this.id, this.image, this.likeCount,this.comments,this.user});
 }
