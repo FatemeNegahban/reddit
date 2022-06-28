6a74c6563a568cce72f6da43326309cfb80728ba
Fateme Negahban <fatemenegahban@example.com>
2022-06-29 01:51:32 +0430

    complet

--- lib/navigation/navigation_bar.dart
+++ lib/navigation/navigation_bar.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:reddit/View/Pages/home_page.dart';
 import 'package:reddit/navigation/navigation_cubit.dart';
+import '../View/pages/Feed/Feed_page.dart';
+import '../View/pages/Search_page.dart';
+
 class NaviBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -10,32 +12,54 @@ class NaviBar extends StatelessWidget {
         create: (context) => NaveCubit(),
         child: BlocBuilder<NaveCubit, int>(builder: (context, state) {
           return Scaffold(
+            appBar: AppBar(
+              title: Text("Search"),
+              actions: [
+                IconButton(
+                    onPressed: () => Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (_) => SearchPage())),
+                    icon: Icon(Icons.search))
+              ],
+            ),
             body: IndexedStack(
               index: state,
               children: [
-                HomePage(),
-                HomePage()
+                FeedPage(),
               ],
             ),
             bottomNavigationBar: BottomNavigationBar(
               currentIndex: state,
               onTap: (index) => context.read<NaveCubit>().selectTab(index),
-              items: [
+              items:[
                 BottomNavigationBarItem(
                   icon: Icon(
-                    Icons.home,
+                    Icons.settings,
+                    color: Colors.black,
                   ),
-                  label: 'Home',
+                  //label: 'setting',
                 ),
 
                 BottomNavigationBarItem(
                   icon: Icon(
                     Icons.home,
+                    color: Colors.black,
                   ),
-                  label: 'feed',
+                  //label: 'feed',
                 ),
-
-
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.add,
+                    color: Colors.black,
+                  ),
+                  //label: 'Add post',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.people,
+                    color: Colors.black,
+                  ),
+                  //label: 'Add post',
+                ),
               ],
             ),
           );
