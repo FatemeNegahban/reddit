e206d93185e4b09a28740bb819f919d398e2a41d
Fateme Negahban <fatemenegahban@example.com>
2022-06-30 21:43:19 +0430

    had changes

---
+++ lib/View/pages/Feed/Feed_cubit.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+enum FeedNavigatorState { home, profile, comments }
+
+class FeedNavigatorCubit extends Cubit<FeedNavigatorState> {
+  FeedNavigatorCubit() : super(FeedNavigatorState.home);
+
+  void showProfile() => emit(FeedNavigatorState.profile);
+  void showComments() => emit(FeedNavigatorState.comments);
+  void showHome() => emit(FeedNavigatorState.home);
+}
