c1cceb1eb2ac34acc377cb4e59d1581335f71fde
Fateme Negahban <fatemenegahban@example.com>
2022-06-30 11:50:09 +0430

    rewrited

---
+++ lib/Login/body.dart
@@ -0,0 +1,73 @@
+import 'dart:developer';
+import 'dart:html';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:reddit/View/pages/Feed/FeedView.dart';
+import 'package:reddit/View/pages/Feed/Feed_page.dart';
+import '../materials/RoundedButton.dart';
+import '../materials/inputField.dart';
+import '../materials/RoundedPassword.dart';
+
+class Body extends StatefulWidget {
+  Function addUser;
+  Body({
+    required Key key,
+    required this.addUser,
+  }) : super(key: key);
+
+  @override
+  State<Body> createState() => _BodyState(addUser : addUser);
+}
+
+class _BodyState extends State<Body> {
+  Function addUser;
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  _BodyState({
+    required this.addUser,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      padding: EdgeInsets.only(left: 20, top: 10),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Login",style: TextStyle(color : Colors.black,fontSize: 23),),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(height: size.height * 0.03),
+                  RoundedInputField(
+                    hintText: "Username",
+                    onChanged: (value) {
+                      _usernameController.text = value;
+                    }, key: Key("username_input"),
+                    hintStyle: Colors.white,
+                  ),
+                  RoundedPasswordField(
+                    onChanged: (value) {
+                      _passwordController.text = value;
+                    }, key: Key("password_input"),
+                  ),
+                  RoundedButton(
+                    text: "Continue",
+                    press: () => Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (_) => FeedView())),
+                    key: Key("continue_input"),
+                  ),
+                  SizedBox(height: size.height * 0.03),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
