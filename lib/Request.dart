ebfc9aef46b7244facae1b43381b03354c08532c
Fatemeh Roshan <famazlom@gmail.com>
2022-07-01 07:36:14 +0430

    request

--- lib/Request.dart
+++ lib/Request.dart
@@ -24,5 +24,24 @@ class Request {
     return listen;
   }
 
-
+  static Future<String> writerListener(String write) async {
+    String listen = '';
+    if (write != null && write.isNotEmpty) {
+      await Socket.connect("192.168.172.187", 1991).then((serverSocket) {
+        write ='user-' + write;
+        serverSocket.write(write);
+        serverSocket.flush();
+        print('write: ' + write);
+        serverSocket.listen((socket) {
+          listen = String.fromCharCodes(socket).trim(); //substring(2);
+        }).onDone(() {
+          print("listen: " + listen);
+        });
+        // serverSocket.close();
+      });
+    } else {
+      print("invalid write");
+    }
+    return listen;
+  }
 }
