8470768984b54a7c755799cb40edf593fee9f9d2
Fatemeh Roshan <famazlom@gmail.com>
2022-07-01 08:01:20 +0430

    account

--- lib/classes/User.dart
+++ lib/classes/User.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:reddit/Request.dart';
 import 'Community.dart';
 import 'Post.dart';
 
@@ -25,7 +27,6 @@ class User {
   void setProfilePic(Object input) {
     profile_pic = input;
   }
-
 
   void addUser(User input) {
     user_list.add(input);
